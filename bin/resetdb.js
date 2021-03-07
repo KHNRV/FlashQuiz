715cd92d5a835fc1347516ec3015beb756e43da2
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-03-07 02:51:56 +0000

    add queries

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -14,10 +14,10 @@ const client = new Client();
 // Loads the schema files from db/schema
 const runSchemaFiles = function () {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync('./db/schema');
+  const schemaFilenames = fs.readdirSync('./db/00_schema');
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./db/00_schema/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
