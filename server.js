1130baf777c0a6c97f6aab92898a21cf8cc90a77
Saïd Redouane <s_red009@hotmail.com>
2021-03-08 22:32:18 +0000

    Make the server leaner

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
+//Include cookie session here instead
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
