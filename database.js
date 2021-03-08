4a4f20613731e65e8522f4b2b0bfb7ef2c3e8f4b
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-03-08 03:48:37 +0000

    add getQuizzes fn

--- database.js
+++ database.js
@@ -1,7 +1,51 @@
 const db = require('./db/index.js');
 
+//TODO: DELETE THIS LATER
+/**
+ * Recreating the function that came with the skeleton
+ * @returns the raw pg query response object
+ */
 const users = function() {
   return db.query('SELECT * FROM users;')
 };
-
 exports.users = users;
+/**
+ * Get all public quizzes, or a users public and private quizzes
+ * @param {number} user_id -  optional
+ * @returns
+ */
+const getQuizzes = function(user_id) {
+  let queryString = `
+    SELECT quizzes.id AS id,
+    users.name AS creator,
+    title,
+    description
+    FROM quizzes
+    JOIN users ON owner_id = users.id
+    WHERE is_active IS TRUE
+  `;
+  if (user_id) {
+    queryString += `AND owner_id = $1;`
+    queryParams = [user_id];
+  } else {
+    queryString += `AND is_public = $1;`;
+    queryParams = ['TRUE'];
+  }
+  return db
+    .query(queryString, queryParams)
+    .then(res => res.rows);
+};
+exports.users = users;
+
+//TODO: remove this before merging
+// --- TEST CODE ---
+
+// --- getQuizzes ---
+
+getQuizzes()
+  .then((data)=>console.log(data)); // expect: an array of 3 objects
+
+getQuizzes(3)
+  .then((data)=>console.log(data)) // expect: an array of 1 objects
+
+
--- db/01_seeds/02_quizzes.sql
+++ db/01_seeds/02_quizzes.sql
@@ -6,3 +6,5 @@ INSERT INTO quizzes (owner_id, title, description)
 VALUES (2, 'Basic Math', 'Test your knowledge of basic math');
 INSERT INTO quizzes (owner_id, title, description)
 VALUES (3, 'Animal Triva', 'Test your knowledge of animals');
+INSERT INTO quizzes (owner_id, title, description, is_public)
+VALUES (3, 'Private Quiz', 'A quiz without any questions', false);
