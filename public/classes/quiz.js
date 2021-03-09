640cb9d1e4bb77044bc6e7bdebe2aa80d35b60ea
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-03-09 20:13:23 +0000

    add custom classes

--- public/classes/quiz.js
+++ public/classes/quiz.js
@@ -9,7 +9,9 @@ class Quiz {
     // Initialize a list of questions
     this.questions = [];
     // Initialize leaderboard
-    this.leaderboard = [];
+    this.globalLeaderboard = [];
+    this.personalLeaderboard = [];
+    this.specifiedLeaderboard = [];
   }
 
   addQuestion(question) {
@@ -24,7 +26,7 @@ class Quiz {
   }
 
   addLeaderboard(leaderboard) {
-    leaderboard.push(entry);
+    leaderboards.push(entry);
   }
 
   getQuizId() {
@@ -49,102 +51,5 @@ class Quiz {
 
 }
 
-class Question {
+module.exports = Quiz;
 
-  constructor(prompt) {
-    this.prompt = prompt;
-    // Initialize a list of answers
-    this.answers = [];
-  }
-
-  addAnswer(answer) {
-    this.answers.push(answer);
-  }
-
-  randomizeAnswers() {
-    return this.Answers.sort(()=>{
-      //Returns a random integer between -1 and 1
-      return Math.floor((Math.random()*3) -1)
-    })
-  }
-
-}
-
-class Answer {
-
-  constructor(answer,is_correct) {
-    this.answer = answer;
-    this.is_correct = is_correct;
-  }
-
-}
-
-
-const quiz = new Quiz('Colours', 'Existential questions about colours', false)
-const question1 = new Question('What colour is the sky?')
-const question2 = new Question('What colour is the sea?')
-const answer1 = new Answer('Blue', true)
-const answer2 = new Answer('Gray', false)
-const answer3 = new Answer('Orange', false)
-const answer4 = new Answer('Yellow', false)
-const answer5 = new Answer('Wine-dark', true)
-const answer6 = new Answer('Blue', false)
-const answer7 = new Answer('Green', false)
-const answer8 = new Answer('Turquoise', false)
-question1.addAnswer(answer1)
-console.log(question1)
-question2.addAnswer(new Answer('Flag',true))
-console.log(question2)
-quiz.addQuestion(new Question('What is my name').addAnswer('Nick', true))
-console.log(quiz)
-
-
-    const object = {
-      title: 'Colours',
-      description: 'The category is colours',
-      is_public: 'FALSE',
-      questions: [
-        {
-          prompt: 'What colour is the sky?',
-          answers: [
-            {
-              answer: 'Blue',
-              is_correct: 'TRUE'
-            },
-            {
-              answer: 'Green',
-              is_correct: 'FALSE'
-            },
-            {
-              answer: 'Yellow',
-              is_correct: 'FALSE'
-            },
-            {
-              answer: 'Violet',
-              is_correct: 'FALSE'
-            }
-          ]
-        },
-        {
-          prompt: 'What colour is the sea?',
-          answers: [
-            {
-              answer: 'Wine-dark',
-              is_correct: 'TRUE'
-            },
-            {
-              answer: 'Green',
-              is_correct: 'FALSE'
-            },
-            {
-              answer: 'Blue',
-              is_correct: 'FALSE'
-            },
-            {
-              answer: 'Turquoise',
-              is_correct: 'FALSE'
-            }
-          ]
-        }
-      ]
-    }
