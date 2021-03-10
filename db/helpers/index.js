953aaee869fb13a1e43832ff83018859756c4dd3
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-10 17:17:59 +0000

    Add undefined handling to fetch quiz
    Co-authored-by: Saïd <Amazigh-SR@users.noreply.github.com>
    Co-authored-by: Nicholas Meisenheimer <souredoutlook@users.noreply.github.com>

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -1,8 +1,19 @@
 //index.js -> all of the data helpers flow through this file
-const { getQuizzes, fetchLeaderboard, fetchQuizDetails, fetchQuizQuestions, addQuiz } = require("./quizData");
-const { fetchUserNameById, getUserByEmail, verifyUserName, verifyEmail, addUser } = require("./userData");
-const { startAttempt, finishAttempt } = require("./attemptData")
-
+const {
+  getQuizzes,
+  fetchLeaderboard,
+  fetchQuizDetails,
+  fetchQuizQuestions,
+  addQuiz,
+} = require("./quizData");
+const {
+  fetchUserNameById,
+  getUserByEmail,
+  verifyUserName,
+  verifyEmail,
+  addUser,
+} = require("./userData");
+const { startAttempt, finishAttempt } = require("./attemptData");
 
 const fetchAssembledQuiz = function(params) {
   // an aggregate function that compiles a quiz object based on parameters
@@ -10,8 +21,8 @@ const fetchAssembledQuiz = function(params) {
 
   const promiseObject = {};
 
-  const quizPromise = fetchQuizDetails(quizId)
-  promiseObject.quiz = quizPromise
+  const quizPromise = fetchQuizDetails(quizId);
+  promiseObject.quiz = quizPromise;
 
   if (questions) {
     const questionsPromise = fetchQuizQuestions(quizId);
@@ -22,7 +33,7 @@ const fetchAssembledQuiz = function(params) {
   promiseObject.globalLeaderboard = globalLeaderboardPromise;
 
   if (userId) {
-    const personalLeaderboardPromise = fetchLeaderboard(quizId,userId);
+    const personalLeaderboardPromise = fetchLeaderboard(quizId, userId);
     promiseObject.personalLeaderboard = personalLeaderboardPromise;
   }
 
@@ -31,19 +42,31 @@ const fetchAssembledQuiz = function(params) {
     promiseObject.specifiedLeaderboard = specifiedLeaderboardPromise;
   }
 
-  return Promise.all(Object.values(promiseObject)).then((response)=>{
+  return Promise.all(Object.values(promiseObject)).then((response) => {
+    if (!response[0]) {
+      return undefined;
+    }
     const keys = Object.keys(promiseObject);
     for (const key of keys) {
-      if (key === 'questions') {
-        for (const question of response[keys.indexOf('questions')]) {
+      if (key === "questions") {
+        for (const question of response[keys.indexOf("questions")]) {
           response[0].addQuestion(question);
         }
-      } else if (key.includes('global')) {
-        response[0].addLeaderboard(response[keys.indexOf('globalLeaderboard')],'globalLeaderboard')
-      } else if (key.includes('personal')) {
-        response[0].addLeaderboard(response[keys.indexOf('personalLeaderboard')],'personalLeaderboard')
-      } else if (key.includes('specified')) {
-        response[0].addLeaderboard(response[keys.indexOf('specifiedLeaderboard')],'specifiedLeaderboard')
+      } else if (key.includes("global")) {
+        response[0].addLeaderboard(
+          response[keys.indexOf("globalLeaderboard")],
+          "globalLeaderboard"
+        );
+      } else if (key.includes("personal")) {
+        response[0].addLeaderboard(
+          response[keys.indexOf("personalLeaderboard")],
+          "personalLeaderboard"
+        );
+      } else if (key.includes("specified")) {
+        response[0].addLeaderboard(
+          response[keys.indexOf("specifiedLeaderboard")],
+          "specifiedLeaderboard"
+        );
       }
     }
     return response[0];
@@ -64,7 +87,6 @@ exports.getUserByEmail = getUserByEmail;
 exports.verifyUserName = verifyUserName;
 exports.verifyEmail = verifyEmail;
 exports.addUser = addUser;
-
 
 // --- attempt data ---
 exports.startAttempt = startAttempt;
