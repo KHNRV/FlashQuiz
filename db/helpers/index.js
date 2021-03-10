22413cdb5be7a351e53645992105fa38887c0530
souredoutlook <souredoutlook@gmail.com>
2021-03-10 02:30:27 -0500

    add fetchAssembledQuiz

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -1,14 +1,58 @@
 //index.js -> all of the data helpers flow through this file
+const { Question } = require('../../public/classes/classes')
 const { getQuizzes, getLeaderboard, fetchQuizDetails, fetchQuizQuestions, addQuiz } = require("./quizData");
 const { fetchUserNameById, getUserByEmail, verifyUserName, verifyEmail, addUser } = require("./userData");
 const { startAttempt, finishAttempt } = require("./attemptData")
 
 
 const fetchAssembledQuiz = function(params) {
+  // an aggregate function that compiles a quiz object based on parameters
   const { questions, quizId, userId, publicId } = params;
-  return "fantastic";
+
+  const promiseObject = {};
+
+  const quizPromise = fetchQuizDetails(quizId)
+  promiseObject.quiz = quizPromise
+
+  if (questions) {
+    const questionsPromise = fetchQuizQuestions(quizId);
+    promiseObject.questions = questionsPromise;
+  }
+
+  const globalLeaderboardPromise = getLeaderboard(quizId);
+  promiseObject.globalLeaderboard = globalLeaderboardPromise;
+
+  if (userId) {
+    const personalLeaderboardPromise = getLeaderboard(quizId,userId);
+    promiseObject.personalLeaderboard = personalLeaderboardPromise;
+  }
+
+  if (publicId) {
+    const specifiedLeaderboardPromise = getLeaderboard(quizId, publicId);
+    promiseObject.specifiedLeaderboard = specifiedLeaderboardPromise;
+  }
+
+  return Promise.all(Object.values(promiseObject)).then((response)=>{
+    const keys = Object.keys(promiseObject);
+    for (const key of keys) {
+      if (key === 'questions') {
+        for (const question of response[keys.indexOf('questions')]) {
+          response[0].addQuestion(question);
+        }
+      } else if (key.includes('global')) {
+        response[0].addLeaderboard(response[keys.indexOf('globalLeaderboard')],'globalLeaderboard')
+      } else if (key.includes('personal')) {
+        response[0].addLeaderboard(response[keys.indexOf('personalLeaderboard')],'personalLeaderboard')
+      } else if (key.includes('specified')) {
+        response[0].addLeaderboard(response[keys.indexOf('specifiedLeaderboard')],'specifiedLeaderboard')
+      }
+    }
+    return response[0];
+  });
 };
 exports.fetchAssembledQuiz = fetchAssembledQuiz;
+
+fetchAssembledQuiz({questions: true, quizId: 1, userId: 1, publicId: 2}).then(data=>console.log(data))
 
 // --- quiz data ---
 exports.getQuizzes = getQuizzes;
@@ -28,11 +72,3 @@ exports.addUser = addUser;
 // --- attempt data ---
 exports.startAttempt = startAttempt;
 exports.finishAttempt = finishAttempt;
-
-// --- test code ---
-
-fetchQuizQuestions(1).then(data=>console.log(data[0]));
-fetchQuizQuestions(0).then(data=>console.log(data));
-
-
-
