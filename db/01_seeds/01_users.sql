02f4f6795cbf5753132e178ddbf3247e11848b48
souredoutlook <souredoutlook@gmail.com>
2021-03-10 15:04:51 +0000

    add hashed passwords to seeds

--- db/01_seeds/01_users.sql
+++ db/01_seeds/01_users.sql
@@ -1,10 +1,10 @@
 -- Users table seeds here
 
 INSERT INTO users (name, email, password)
-VALUES ('Said','said@flashquiz.ca','password');
+VALUES ('Said','said@flashquiz.ca','$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO');
 --! sorry Saïd our database does not seem to support charCodes past 255
 INSERT INTO users (name, email, password)
-VALUES ('Kevin','kevin@flashquiz.ca', 'password');
+VALUES ('Kevin','kevin@flashquiz.ca', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO');
 INSERT INTO users (name, email, password)
-VALUES ('Nick','nick@flashquiz.ca','password');
+VALUES ('Nick','nick@flashquiz.ca','$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO');
 
