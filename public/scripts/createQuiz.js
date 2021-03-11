c9ce13f822eee5b0f08de5a069cdc6cdd0481512
souredoutlook <souredoutlook@gmail.com>
2021-03-11 12:40:35 +0000

    refactor addQuiz

--- public/scripts/createQuiz.js
+++ public/scripts/createQuiz.js
@@ -5,56 +5,65 @@ const escapeText = function(str) {
   return element.html();
 };
 
+const submitQuiz = function(event) {
+  event.preventDefault();
+
+  // store quiz information
+  const title = $("[name='title']").val();
+  const description = $("[name='description']").val();
+  const isPublic = $("[type='checkbox']").prop("checked");
+  const prompts = $("[name='prompt']");
+  const correctAnswers = $("[name='correct']");
+  const wrongAnswers1 = $("[name='wrong_1']");
+  const wrongAnswers2 = $("[name='wrong_2']");
+  const wrongAnswers3 = $("[name='wrong_3']");
+
+  const quiz = new Quiz(title, description, !isPublic);
+
+  //iterate over jQuery "arrays" and create associated classes as they are added to the quiz
+  for (let i = 0; i < prompts.length; i++) {
+    const question = new Question($(prompts[i]).val());
+    question.addAnswer(new Answer($(correctAnswers[i]).val(), true));
+    question.addAnswer(new Answer($(wrongAnswers1[i]).val(), false));
+    question.addAnswer(new Answer($(wrongAnswers2[i]).val(), false));
+    question.addAnswer(new Answer($(wrongAnswers3[i]).val(), false));
+    quiz.addQuestion(question);
+  }
+
+  $.ajax({
+    url: "/quizzes",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(quiz),
+  }).done(() => {
+    // turn off event handlers for #new_quiz, trigger a form reset, then trigger submit (default form behaviour makes a GET request to /quizzes)
+    $("#new_quiz").off();
+    $("#new_quiz").trigger("reset");
+    $("#new_quiz").trigger("submit");
+  });
+};
+
+const addQuestionCard = function(counter) {
+  counter += 1;
+  $("#questions_container").append(questionCardTemplate(counter));
+};
+
+let counter = 0;
+
 $(() => {
   $("#new_quiz").on("submit", (event) => {
-    event.preventDefault();
-
-    const title = $("[name='title']").val();
-    const description = $("[name='description']").val();
-    const isPublic = $("[type='checkbox']").prop("checked");
-
-    const quiz = new Quiz(title, description, isPublic);
-
-    const prompts = $("[name='prompt']");
-    const correctAnswers = $("[name='correct']");
-    const wrongAnswers1 = $("[name='wrong_1']");
-    const wrongAnswers2 = $("[name='wrong_2']");
-    const wrongAnswers3 = $("[name='wrong_3']");
-
-    // console.log(Object.keys(prompts));
-    // console.log(prompts);
-    for (let i = 0; i < prompts.length; i++) {
-      const question = new Question($(prompts[i]).val());
-      question.addAnswer(new Answer($(correctAnswers[i]).val(), true));
-      question.addAnswer(new Answer($(wrongAnswers1[i]).val(), false));
-      question.addAnswer(new Answer($(wrongAnswers2[i]).val(), false));
-      question.addAnswer(new Answer($(wrongAnswers3[i]).val(), false));
-      quiz.addQuestion(question);
-    }
-
-    $.ajax({
-      url: "/quizzes",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(quiz),
-    }).done(() => {
-      $("#new_quiz").off();
-      $("#new_quiz").trigger("reset");
-      $("#new_quiz").trigger("submit");
-    });
+    submitQuiz(event);
   });
 
-  //add a question card and initialize couter on load
-  let counter = 1;
-  $("#questions_container").append(questionCardTemplate(counter));
+  //add a question card and initialize counter on load
+  addQuestionCard(counter);
 
-  //add more questions
+  //add question card
   $("#add_question").click(() => {
-    counter += 1;
-    $("#questions_container").append(questionCardTemplate(counter));
+    addQuestionCard(counter);
   });
 
-  //delete a question
+  //delete a question card from the container
   $("#questions_container").on("click", ".delete_question", function(event) {
     $(event.target).parents(".create_card").remove();
   });
