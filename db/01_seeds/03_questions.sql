110e9732dba254c6e03667e0718470592f044a34
souredoutlook <souredoutlook@gmail.com>
2021-03-10 21:05:30 -0500

    add Kevin's awesome quiz to the db

--- db/01_seeds/03_questions.sql
+++ db/01_seeds/03_questions.sql
@@ -23,3 +23,11 @@ INSERT INTO questions (quiz_id, prompt) VALUES
 (3, 'Which animal is the largest land mammal in North America?'), -- Bison
 (3, 'How much did the heaviest Giant Tortoise weigh?'), --250kg
 (3, 'From how far away can the roar of a Lion be heard?'); --5 km
+
+--question.id 16 - 19, quiz_id = 5
+INSERT INTO questions (quiz_id, prompt) VALUES
+(5, "From 1964 to 1985, who asked you not to squeeze the Charmin?"),
+(5,  "Nadia Comaneci was the first gymnast to ever do what at the Olympics?"),
+(5, "Which of these birds has the biggest brain relative to its size?"),
+(5, "When playing Blackjack, how many points would be considered a bust?")
+
