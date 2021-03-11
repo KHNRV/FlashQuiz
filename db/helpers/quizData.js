b2ee720d86b7362c4ac78809e010feeedf9efc91
souredoutlook <souredoutlook@gmail.com>
2021-03-11 07:51:18 -0500

    change output of leaderboard query

--- db/helpers/quizData.js
+++ db/helpers/quizData.js
@@ -48,21 +48,20 @@ const fetchLeaderboard = function(quiz_id, user_id) {
   SELECT name,
   concat(num_correct * 100 / total_questions, '%') AS accuracy,
   to_char(duration, 'MI:SS') AS time,
-  num_correct * (date_part('microsecond', (duration) / max_duration)) AS computed_score
-  FROM (
-    SELECT
-    attempts.id,
-    name,
-    end_time - start_time AS duration,
-    SUM(questions.time_limit / 1000) max_duration,
-    num_correct,
-    COUNT(questions.*) AS total_questions
-    FROM attempts
-    JOIN quizzes ON attempts.quiz_id = quizzes.id
-    JOIN questions ON questions.quiz_id = quizzes.id
-    JOIN users ON attempts.user_id = users.id
-    WHERE end_time IS NOT NULL AND quizzes.id = $1
-
+  floor((((num_correct / total_questions) * 800000) + (num_correct * (1000000 - (date_part('microsecond', (duration) / max_duration)))))*.01) AS computed_score
+    FROM (
+      SELECT
+      attempts.id,
+      name,
+      end_time - start_time AS duration,
+      SUM(questions.time_limit / 1000) max_duration,
+      num_correct,
+      COUNT(questions.*) AS total_questions
+      FROM attempts
+      JOIN quizzes ON attempts.quiz_id = quizzes.id
+      JOIN questions ON questions.quiz_id = quizzes.id
+      JOIN users ON attempts.user_id = users.id
+      WHERE end_time IS NOT NULL AND quizzes.id = $1
   `;
   if (user_id) {
     queryParams.push(user_id);
