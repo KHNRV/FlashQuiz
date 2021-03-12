62e40d6b9821066cd62f6617fe6a016b7cbb855c
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-12 00:37:44 +0000

    Add function for to send result of a quiz attempt

--- db/helpers/attemptData.js
+++ db/helpers/attemptData.js
@@ -1,4 +1,4 @@
-const db = require("../pool")
+const db = require("../pool");
 
 /**
  * Insert an attempt into the attempts table
@@ -6,21 +6,19 @@ const db = require("../pool")
  * @param {number} user_id - mandatory
  * @returns {object} attempt_id
  */
- const startAttempt = function(quiz_id, user_id) {
+const startAttempt = function(quiz_id, user_id) {
   const queryParams = [quiz_id, user_id];
   const queryString = `INSERT INTO attempts (quiz_id, user_id) VALUES ($1,$2) RETURNING *;`;
   return db.query(queryString, queryParams).then((res) => {
-    return { attempt_id: res.rows[0].id };
+    return res.rows[0].id;
   });
 };
 
-
-//TODO: change what this sends back based on front-end requirements
 /**
  * Update an attempt with number of correct answers
  * @param {number} num_correct - mandatory
  * @param {number} attempt_id - mandatory
- * @returns {object} raw query response object
+ * @returns {object} attempt result objet
  */
 const finishAttempt = function(num_correct, attempt_id) {
   queryParams = [num_correct, attempt_id];
@@ -28,11 +26,41 @@ const finishAttempt = function(num_correct, attempt_id) {
     UPDATE attempts
     SET end_time = NOW()::TIMESTAMP,
     num_correct = $1
-    WHERE id = $2;
+    WHERE id = $2
+    RETURNING *;
   `;
-  return db.query(queryString, queryParams).then((res) => {
-    return res;
-  });
+  return db
+    .query(queryString, queryParams)
+    .then((res) => {
+      return res.rows[0].id;
+    })
+    .then((attempt_id) => {
+      const queryParams = [attempt_id];
+      let queryString = `
+    SELECT name,
+    concat(num_correct * 100 / total_questions, '%') AS accuracy,
+    to_char(duration, 'MI:SS') AS time,
+    floor((((num_correct / total_questions) * 800000) + (num_correct * (1000000 - (date_part('microsecond', (duration) / max_duration)))))*.01) AS computed_score
+      FROM (
+        SELECT
+        attempts.id,
+        name,
+        end_time - start_time AS duration,
+        SUM(questions.time_limit / 1000) max_duration,
+        num_correct,
+        COUNT(questions.*) AS total_questions
+        FROM attempts
+        JOIN quizzes ON attempts.quiz_id = quizzes.id
+        JOIN questions ON questions.quiz_id = quizzes.id
+        JOIN users ON attempts.user_id = users.id
+        WHERE attempts.id = $1
+        GROUP BY attempts.id, name, end_time - start_time, num_correct
+      ) AS query
+      ORDER BY computed_score DESC;
+    `;
+      return db.query(queryString, queryParams);
+    })
+    .then((res) => res.rows[0]);
 };
 
-module.exports = { startAttempt, finishAttempt}
+module.exports = { startAttempt, finishAttempt };
